c6d4b6cded8c47b242f3bc8e223245b6ebe584b4
Tobias Ara Svensson <tobias.ara.svensson@meltwater.com>
2016-08-01 12:12:10 +0200

    Release version 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LogstashJson.Mixfile do
 
   def project do
     [app: :logstash_json,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,

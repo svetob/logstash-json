bb9b80f8fa87523f51829507168d4d94003e42f5
Tobias Ara Svensson <tobias.ara.svensson@meltwater.com>
2016-08-24 21:01:14 +0200

    Convert messages to string

--- test/event_test.exs
+++ test/event_test.exs
@@ -5,38 +5,51 @@ defmodule EventTest do
   test "Creates and serializes event" do
     message = "Meow meow"
     event = log(message)
-      |> Poison.decode!()
 
-    assert Map.get(event, "message") == message
-    assert Map.get(event, "level") == "info"
-    assert Map.get(event, "metadata") == []
-    assert String.length(Map.get(event, "@timestamp")) == 29
+    time = Map.get(event, :"@timestamp")
+
+    assert Map.get(event, :message) == message
+    assert Map.get(event, :level) == :info
+    assert Map.get(event, :metadata) == []
+    assert String.length(time) == 29
   end
 
   test "Joins extra fields but does not overwrite existing fields" do
     message = "Meow the second"
     event = log(message, %{foo: "bar", level: "fail", message: "fail"})
-      |> Poison.decode!()
+
+    assert Map.get(event, :message) == message
+    assert Map.get(event, :level) == :info
+    assert Map.get(event, :foo) == "bar"
+  end
+
+  test "Converts event to json" do
+    message = "Meowson the third"
+    event = log_json(message) |> Poison.decode!()
 
     assert Map.get(event, "message") == message
     assert Map.get(event, "level") == "info"
-    assert Map.get(event, "foo") == "bar"
   end
 
-  test "Handle construct [1, 2 | 3]" do
+  test "Formats message" do
+    assert log(["Hello", 32, 'wo', ["rl", 'd!']])
+      |> Map.get(:message) == "Hello world!"
+  end
+
+  test "Handle lists such as [1, 2 | 3]" do
     assert log(["a", "b" | "c"])
-      |> Poison.decode!()
-      |> Map.get("message") == ["a", "b", "c"]
+      |> Map.get(:message) == "abc"
   end
 
-  defp log(msg) do
-    log(msg, %{})
-  end
-  defp log(msg, fields) do
-    {:ok, l} = Event.event(:info, msg, {{2015,1,1},{0,0,0,0}}, [], %{
+  defp log(msg, fields \\ %{}) do
+    Event.event(:info, msg, {{2015,1,1},{0,0,0,0}}, [], %{
       metadata: [],
       fields: fields
-      }) |> Event.json()
+    })
+  end
+
+  defp log_json(msg, fields \\ %{}) do
+    {:ok, l} = Event.json(log(msg, fields))
     l
   end
 end

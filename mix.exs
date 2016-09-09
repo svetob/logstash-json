d70c2ed1bc0cb4946118674ee96a82583bb940eb
Tobias Ara Svensson <tobias.ara.svensson@meltwater.com>
2016-09-09 22:34:01 +0200

    Set version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LogstashJson.Mixfile do
 
   def project do
     [app: :logstash_json,
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.3",
      description: description(),
      package: package(),

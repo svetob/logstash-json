495a9f03287576e778c5f393f5581eb338f233c3
Tobias Ara Svensson <tobias.ara.svensson@meltwater.com>
2017-08-16 09:37:40 +0200

    Set version to 0.6.0

--- mix.exs
+++ mix.exs
@@ -2,14 +2,16 @@ defmodule LogstashJson.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :logstash_json,
-     version: "0.5.2",
-     elixir: "~> 1.4",
-     description: description(),
-     package: package(),
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :logstash_json,
+      version: "0.6.0",
+      elixir: "~> 1.4",
+      description: description(),
+      package: package(),
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
   end
 
   def application do
@@ -17,11 +19,13 @@ defmodule LogstashJson.Mixfile do
   end
 
   defp deps do
-    [{:connection, "~> 1.0.4"},
-     {:poison, ">= 1.0.0"},
-     {:blocking_queue, "~> 1.3.0"},
-     {:ex_doc, ">= 0.0.0", only: :dev},
-     {:credo, ">= 0.0.0", only: :dev}]
+    [
+      {:connection, "~> 1.0.4"},
+      {:poison, ">= 1.0.0"},
+      {:blocking_queue, "~> 1.3.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:credo, ">= 0.0.0", only: :dev}
+    ]
   end
 
   defp description do
@@ -31,7 +35,8 @@ defmodule LogstashJson.Mixfile do
   end
 
   defp package do
-    [ name: :logstash_json,
+    [
+      name: :logstash_json,
       maintainers: ["Tobias Ara Svensson"],
       licenses: ["MIT"],
       links: %{"Github" => "https://github.com/svetob/logstash-json"},

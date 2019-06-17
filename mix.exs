6562bbc6c5d71f94246fc29d96d5165bc255079d
Tobias Ara Svensson <tobias@forzafootball.com>
2019-06-17 14:16:34 +0200

    Set version to 0.7.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LogstashJson.Mixfile do
   def project do
     [
       app: :logstash_json,
-      version: "0.7.3",
+      version: "0.7.4",
       elixir: "~> 1.4",
       description: description(),
       package: package(),

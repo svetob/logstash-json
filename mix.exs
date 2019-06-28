a5bdbfb1e30539c2a7f46d4247bcc560e1a4f837
Ilja Tkachuk <ilja@heathmont.net>
2019-06-28 17:26:56 +0300

    print_pids -> pre_encode, check for custom Poison.Encoder protocol implementation for structs

--- mix.exs
+++ mix.exs
@@ -10,6 +10,7 @@ defmodule LogstashJson.Mixfile do
       package: package(),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
+      consolidate_protocols: Mix.env() != :test,
       deps: deps()
     ]
   end

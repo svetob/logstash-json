63ae7897bf351b4060874dd46ebbf794310052ff
Ilja Tkachuk <ilja@heathmont.net>
2019-07-01 10:57:05 +0300

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LogstashJson.Mixfile do
   def project do
     [
       app: :logstash_json,
-      version: "0.7.4",
+      version: "0.7.5",
       elixir: "~> 1.4",
       description: description(),
       package: package(),
